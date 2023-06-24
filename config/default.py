e9fdc15be8fbefc958d6d1089de9c7a87d93cc74
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-06-24 16:42:14 +0700

    convert parquet to csv - ưith POLARS

--- config/default.py
+++ config/default.py
@@ -3,4 +3,4 @@ import os
 password = os.environ.get('NY_TAXI_PASSWORD')
 POSTGRES_CONN_STRING = f'postgresql://datalake_user:{password}@localhost:5432/ny_taxi_data'
 
-print(password)
+DATA_PATH = '/home/tb24/projects/Datalake-project/data'
--- explore/parquet_to_csv.py
+++ explore/parquet_to_csv.py
@@ -1,7 +1,8 @@
 from ftplib import FTP
+from config.default import DATA_PATH
 import pandas as pd
+import polars as pl
 import os
-
 
 ftp_ip = '192.168.1.10'
 ftp_usr = 'bindz'
@@ -10,20 +11,37 @@ ftp_pwd = '240202'
 ftp = FTP(ftp_ip)
 ftp.login(ftp_usr, ftp_pwd)
 
-# Download file from FTP server
-# filename = 'taxi+_zone_lookup.csv'
-# with open(filename, 'wb') as file:
-#     ftp.retrbinary('RETR ' + filename, file.write)
 filenames = ftp.nlst()
 for filename in filenames:
     if filename.endswith('.parquet'):
-        local_filename = os.path.join('/home/tb24/projects/Datalake-project/data', filename)
+        local_filename = os.path.join(DATA_PATH, filename)
         file = open(local_filename, 'wb')
         ftp.retrbinary('RETR ' + filename, file.write)
 
 ftp.quit()
 
-# Thay ten file parquet o day
-# df = pd.read_parquet('data/parquet/parquet_example.parquet')
-# df.to_csv('data/csv/parquet_example.csv', index=False)
+Convert parquet to csv for spooldir
+all_files = os.listdir(DATA_PATH)
+parquet_files = []
+for file in all_files:
+    if file.endswith('.parquet'):
+        parquet_files.append(file)
+print(parquet_files)
 
+for name in parquet_files:
+    file_name = os.path.splitext(name)[0]
+    df = pd.read_parquet(os.path.join(DATA_PATH,name))
+    df.to_csv(os.path.join(DATA_PATH, f'{file_name}.csv'), index=False)
+
+# using POLARS - demn its BLAZINGLY FAST
+all_files = os.listdir(DATA_PATH)
+parquet_files = []
+for file in all_files:
+    if file.endswith('.parquet'):
+        parquet_files.append(file)
+print(parquet_files)
+
+for name in parquet_files:
+    file_name = os.path.splitext(name)[0]
+    df = pl.read_parquet(os.path.join(DATA_PATH,name))
+    df.write_csv(os.path.join(DATA_PATH, f'{file_name}.csv'))
