2ec242b959fb735070ace9b0d08df81215cdf029
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-06-24 14:56:08 +0700

    minor changes

--- explore/parquet_to_csv.py
+++ explore/parquet_to_csv.py
@@ -1,5 +1,27 @@
+from ftplib import FTP
 import pandas as pd
 
+
+ftp_ip = '192.168.1.10'
+ftp_usr = 'bindz'
+ftp_pwd = '240202'
+
+ftp = FTP(ftp_ip)
+ftp.login(ftp_usr, ftp_pwd)
+
+# Download file from FTP server
+# filename = 'taxi+_zone_lookup.csv'
+# with open(filename, 'wb') as file:
+#     ftp.retrbinary('RETR ' + filename, file.write)
+files = ftp.nlst()
+for file in files:
+    if file.endswith('.parquet'):
+        with open(file, 'wb') as f:
+            ftp.retrbinary('RETR' + file, f.write)
+
+ftp.quit()
+
 # Thay ten file parquet o day
-df = pd.read_parquet('data/parquet/parquet_example.parquet')
-df.to_csv('data/csv/parquet_example.csv', index=False)
+# df = pd.read_parquet('data/parquet/parquet_example.parquet')
+# df.to_csv('data/csv/parquet_example.csv', index=False)
+
