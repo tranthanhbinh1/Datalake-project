c0f39f7c71387d21dd0b195ce290065f48c99fde
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-06-26 23:01:39 +0700

    add Kafka producer and Spark Structured Streaming

--- streaming/dbToS3.py
+++ streaming/dbToS3.py
@@ -1,1 +1,32 @@
 from kafka import KafkaProducer
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import from_json, col
+
+
+# Define the Kafka producer , RUN DOCKER COMPOSE UP FIRST
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+# Define the Kafka broker and topic
+bootstrap_server = "localhost:9092"
+topic = "stock_analyzer"
+
+spark = SparkSession.builder.appName("StockDashboard").getOrCreate()
+
+# Define the schema for the stock data
+schema = "value string"
+
+df = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", bootstrap_server) \
+  .option("subscribe", topic) \
+  .load()
+
+# Convert the value column from binary to string
+df = df.withColumn("value", df["value"].cast("string"))
+
+# Print the data to the console
+query = df.writeStream.outputMode("append").format("console").start()
+
+# Wait for the termination of the streaming query
+query.awaitTermination()
