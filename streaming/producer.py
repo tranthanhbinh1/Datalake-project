178ada69a5339d3e914449266f8f58f4f6c1f61a
TranThanhBinh1 <tranngocthanhbinh01@gmail.com>
2023-06-28 15:40:15 +0700

    minor changes

--- streaming/producer.py
+++ streaming/producer.py
@@ -8,3 +8,12 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 bootstrap_servers = "localhost:29092"
 topic1 = "dbToS3"
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+def publishToKafka(topic, data):
+    try:
+        producer.send(topic, str.encode(data))
+        producer.flush()
+        print("Message published successfully to Kafka")
+    except Exception as ex:
+        print("Exception in publishing message to Kafka")
+        print(str(ex))
